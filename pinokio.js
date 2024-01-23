dc22e6e8581f3659f9389e5cf24671fb31a107fd
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-23 16:44:08 -0500

    init

---
+++ pinokio.js
@@ -0,0 +1,64 @@
+const path = require('path')
+module.exports = {
+  version: 1,
+  title: "Moondream1",
+  description: "moondream1 is a tiny (1.6B parameter) vision language model trained by @vikhyatk that performs on par with models twice its size. It is trained on the LLaVa training dataset, and initialized with SigLIP as the vision tower and Phi-1.5 as the text encoder. https://huggingface.co/spaces/vikhyatk/moondream1",
+  icon: "icon.png",
+  menu: async (kernel) => {
+    let installing = await kernel.running(__dirname, "install.js")
+    let installed = await kernel.exists(__dirname, "env")
+    let running = await kernel.running(__dirname, "start.js")
+    if (installing) {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
+      if (running) {
+        let local = kernel.memory.local[path.resolve(__dirname, "start.js")]
+        if (local && local.url) {
+          return [{
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        } else {
+          return [{
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        }
+      } else {
+        return [{
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-solid fa-broom",
+          text: "Factory Reset",
+          href: "reset.js",
+        }]
+      }
+    } else {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
+    }
+  }
+}
